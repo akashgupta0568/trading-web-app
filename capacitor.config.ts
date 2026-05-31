a181fdb13f0d3c41d66e2328c0a4492f3c48cfb5
akashgupta0568 <akashgupta.ag0568@gmail.com>
2026-05-31 09:33:04 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'trading-web-app',
+  webDir: 'www'
+};
+
+export default config;
